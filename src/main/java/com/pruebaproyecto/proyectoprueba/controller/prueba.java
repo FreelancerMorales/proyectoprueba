b0f5015714f6ad02a4cbbaf00d5d31384ee4dbb3
Antonio Morales <camoralesc52@gmail.com>
2024-09-25 22:50:40 -0600

    first commit

---
+++ src/main/java/com/pruebaproyecto/proyectoprueba/controller/prueba.java
@@ -0,0 +1,15 @@
+package com.pruebaproyecto.proyectoprueba.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api")
+public class prueba {
+
+    @GetMapping("")
+    public String prueba_hola(){
+        return "Hola sonso";
+    }
+}
---
+++ src/test/java/com/pruebaproyecto/proyectoprueba/ProyectopruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaproyecto.proyectoprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

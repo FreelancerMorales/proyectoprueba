b07df26894c8ec719d2c4d4e206dd70cb54c7098
Antonio Morales <camoralesc52@gmail.com>
2024-09-26 20:00:01 -0600

    Inicialización de Repository listo

---
+++ src/main/java/com/pruebaproyecto/proyectoprueba/repository/productosRepository.java
@@ -0,0 +1,8 @@
+package com.pruebaproyecto.proyectoprueba.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.pruebaproyecto.proyectoprueba.models.productosModel;
+
+public interface productosRepository extends CrudRepository<productosModel, Object> {
+
+}

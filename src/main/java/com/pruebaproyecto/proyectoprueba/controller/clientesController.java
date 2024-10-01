e4324e32773bf2b7f8cf62867b5ca255d22ce1f6
Antonio Morales <camoralesc52@gmail.com>
2024-09-30 21:51:06 -0600

    Arreglos Finales

--- src/main/java/com/pruebaproyecto/proyectoprueba/controller/clientesController.java
+++ src/main/java/com/pruebaproyecto/proyectoprueba/controller/clientesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -31,7 +32,7 @@ public class clientesController {
         return this.cS.findAll();
     }
     
-    @GetMapping("/guardar")
+    @PostMapping("/guardar")
     public ResponseEntity<String> saveClientes(@RequestBody clientesModel entity) {
         try {
             this.cS.save(entity);
